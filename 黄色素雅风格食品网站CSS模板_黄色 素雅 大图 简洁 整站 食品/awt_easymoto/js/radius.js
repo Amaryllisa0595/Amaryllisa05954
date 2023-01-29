986de82b688e20933b697f62bc20d7b5d3863eaf
Amaryllisa0595 <qushu8rt@126.com>
2023-01-29 22:26:24 +0800

    d

---
+++ 黄色素雅风格食品网站CSS模板_黄色 素雅 大图 简洁 整站 食品/awt_easymoto/js/radius.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){ 	
+						   
+						   
+	// radius Box
+	$('.menu_nav').css({"border-top-left-radius": "16px", "-moz-border-radius-topleft":"16px", "-webkit-border-top-left-radius":"16px", "border-top-right-radius": "16px", "-moz-border-radius-topright":"16px", "-webkit-border-top-right-radius":"16px"});
+	// end radius Box
+	 
+});	
